afd5dda42effec3a552add897f87dd1a6ebed201
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-07-11 10:46:30 +0700

    method penyewa_load

--- FinalProject_SewaMobil/Penyewa.cs
+++ FinalProject_SewaMobil/Penyewa.cs
@@ -22,6 +22,31 @@ namespace FinalProject_SewaMobil
             InitializeComponent();
         }
 
+        private void Penyewa_Load()
+        {
+            koneksi.Open();
+            SqlDataAdapter dataAdapter1 = new SqlDataAdapter(new SqlCommand("Select m.id_penyewa, m.nm_penyewa, "
+                + "m.adr_penyewa, m.identitas, m.telp_penyewa, p.id_kry From dbo.penyewa m " +
+                "join dbo.karyawan p on m.id_kry = p.id_kry", koneksi));
+            DataSet ds = new DataSet();
+            dataAdapter1.Fill(ds);
+
+            this.customerBindingSource.DataSource = ds.Tables[0];
+            this.idpenyewa.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "id_penyewa", true));
+            this.txtnama.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "nm_penyewa", true));
+            this.txtadr.DataBindings.Add(
+               new Binding("Text", this.customerBindingSource, "adr_penyewa", true));
+            this.cbxidentity.DataBindings.Add(
+               new Binding("Text", this.customerBindingSource, "identitas", true));
+            this.txttelp.DataBindings.Add(
+               new Binding("Text", this.customerBindingSource, "telp_penyewa", true));
+            this.cbxkrywn.DataBindings.Add(
+               new Binding("Text", this.customerBindingSource, "id_kry", true));
+            koneksi.Close();
+        }
+
         private void label1_Click(object sender, EventArgs e)
         {
 
