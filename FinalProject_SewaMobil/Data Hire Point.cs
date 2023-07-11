173a56ce93e91bbc41d8809650f7c3f1f7f289a7
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-07-11 20:51:43 +0700

    deklarasi string connection

--- FinalProject_SewaMobil/Data Hire Point.cs
+++ FinalProject_SewaMobil/Data Hire Point.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace FinalProject_SewaMobil
 {
     public partial class Data_Hire_Point : Form
     {
+        private string stringConnection = "data source=LAPTOP-9VURLJFC\\THARIQ_AZHAR;" + "database=SewaMobil;User ID=sa;Password=hurricane95";
+        private SqlConnection koneksi;
+        private string id_hp, cabang, nama, telp, adr;
+        BindingSource customerBindingSource = new BindingSource();
         public Data_Hire_Point()
         {
             InitializeComponent();
